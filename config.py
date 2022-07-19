76e9f67e35fa2f4a282f3120a4aae0d1fd9a1826
ayamkentaki <108415240+ayamkentaki@users.noreply.github.com>
2022-07-20 00:36:28 +0700

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ CMD_HANDLER = getenv("CMD_HANDLER", ".")
 DB_URL = getenv("DATABASE_URL", "")
 GIT_TOKEN = getenv(
     "GIT_TOKEN",
-    b64decode("Z2hwXzg1Z1I1RlRRanRRTG9TU2FaRGVrcnI4c1pBSDB6cjRRMlN5UA==").decode(
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL2F5YW1rZW50YWtpL2p1cmFnYW4vYXJjaGl2ZS9tYXN0ZXIuemlw").decode(
         "utf-8"
     ),
 )
