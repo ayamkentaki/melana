57097eef86b5ac16e71614c35a29b05bf65efd49
ayamkentaki <108415240+ayamkentaki@users.noreply.github.com>
2022-07-20 01:03:49 +0700

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ CMD_HANDLER = getenv("CMD_HANDLER", ".")
 DB_URL = getenv("DATABASE_URL", "")
 GIT_TOKEN = getenv(
     "GIT_TOKEN",
-    b64decode("aHR0cHM6Ly9naXRodWIuY29tL2F5YW1rZW50YWtpL2p1cmFnYW4vYXJjaGl2ZS9tYXN0ZXIuemlw").decode(
+    b64decode("Z2hwXzg1Z1I1RlRRanRRTG9TU2FaRGVrcnI4c1pBSDB6cjRRMlN5UA==").decode(
         "utf-8"
     ),
 )
@@ -43,7 +43,7 @@ PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
 PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
 REPO_URL = getenv(
     "REPO_URL",
-    b64decode("aHR0cHM6Ly9naXRodWIuY29tL2F5YW1rZW50YWtpL21lbGFuYQ==").decode("utf-8"),
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL2F5YW1rZW50YWtpL2p1cmFnYW4=").decode("utf-8"),
 )
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
